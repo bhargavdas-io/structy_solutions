63cdfd8a93efad676795f9e790799342b792391e
Bhargav Das <55290652+bhargavdas-io@users.noreply.github.com>
2026-09-21 13:17:29 +0530

    Solved 'reverse list' (python3)

--- Linked-List-I/046-reverse-list/reverse-list.py
+++ Linked-List-I/046-reverse-list/reverse-list.py
@@ -3,10 +3,20 @@
 #     self.val = val
 #     self.next = None
 
-def reverse_nodes(head):
-    if not head or not head.next:
-        return head
-    new_head = reverse_nodes(head.next)
-    head.next.next = head
-    head.next = None
-    return new_head
+# def reverse_list(head):
+#     if not head or not head.next:
+#         return head
+#     new_head = reverse_list(head.next)
+#     head.next.next = head
+#     head.next = None
+#     return new_head
+
+def reverse_list(head):
+    current = head
+    previous = None
+    while current != None:
+        next_node = current.next
+        current.next = previous
+        previous = current
+        current = previous.next
+    return previous
